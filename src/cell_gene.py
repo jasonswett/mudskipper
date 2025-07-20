129c9697a3c3104603774fca0be5eff72c7240c0
Jason Swett <jason@benfranklinlabs.com>
2025-07-20 09:14:35 -0400

    Add cell type.

--- src/cell_gene.py
+++ src/cell_gene.py
@@ -14,6 +14,18 @@ class CellGene(Gene):
     def __init__(self, value_or_length):
         super().__init__(value_or_length)
 
+    def delta_section(self):
+        return self.value[:3]
+
+    def cell_type_section(self):
+        return self.value[3:5]
+
     def delta(self):
-        delta_index = int(self.value, 2)
+        delta_index = int(self.delta_section(), 2)
         return self.LEGAL_DELTAS[delta_index]
+
+    def cell_type(self):
+        if self.cell_type_section() == "11":
+            return "pulser"
+        else:
+            return "default"
--- tests/test_cell_gene.py
+++ tests/test_cell_gene.py
@@ -10,3 +10,9 @@ def test_cell_gene_initialization():
     assert CellGene("100").delta() == (0, 1, -1)
     assert CellGene("101").delta() == (-1, 1, 0)
     assert CellGene("110").delta() == (-1, 0, 1)
+
+def test_cell_type():
+    assert CellGene("00000").cell_type() == "default"
+    assert CellGene("00001").cell_type() == "default"
+    assert CellGene("00010").cell_type() == "default"
+    assert CellGene("00011").cell_type() == "pulser"
