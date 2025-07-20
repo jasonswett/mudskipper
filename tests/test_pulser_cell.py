6e6ecd051fa179ee08ef366dc515532c48be1439
Jason Swett <jason@benfranklinlabs.com>
2025-07-20 15:54:27 -0400

    Remove now-unneeded mock.

--- tests/test_pulser_cell.py
+++ tests/test_pulser_cell.py
@@ -1,12 +1,11 @@
 import pytest
 from src.pulser_cell import PulserCell
-from unittest.mock import Mock
+from src.cellular_body import CellularBody
 
 def test_clock_tick():
-    cellular_body = Mock()
-    cellular_body.neighbors.return_value = []
     original_fill_color = (0, 0, 255)
     pulser_cell = PulserCell((0, 0, 0), 1, (0, 0, 0), original_fill_color)
+    cellular_body = CellularBody([pulser_cell])
 
     for i in range(PulserCell.PULSE_INTERVAL):
         pulser_cell.update_clock(cellular_body)
