f6952c056398c7952512f45840495d0e9075f2ef
Jason Swett <jason@benfranklinlabs.com>
2025-07-19 23:44:24 -0400

    Add test for y.

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -17,5 +17,5 @@ def test_cell_coordinate_relationships():
     radius = 2
     cell1 = Cell((0, 0, 0), radius, (0, 255, 0))
     cell2 = Cell((1, 0, -1), radius, (0, 255, 0))
-    
     assert cell2.center_x() - cell1.center_x() == 3
+    assert cell2.center_y() - cell1.center_y() == radius * math.sqrt(3) / 2
